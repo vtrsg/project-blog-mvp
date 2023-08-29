2e11282b8653fad7761111e5dbbe965719938d1a
vtrsg <vittorioschmittg@gmail.com>
2023-08-29 17:26:39 -0300

    (feat)[feature/create-view-post]: isolating responsability add image

--- blog_mvp/api/models.py
+++ blog_mvp/api/models.py
@@ -5,10 +5,11 @@ from django.utils import timezone
 # import user model django
 from django.contrib.auth.models import User
 # import utils
-from utils.utils_models import upload_image_to
+#from utils.utils_models import upload_image_to
+import os
 
 class Tag(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, null=False, blank=False)
     slug = models.SlugField(unique=True, default=None, null=True, blank=True, max_length=255)
 
     def save(self, *args, **kwargs):
@@ -20,7 +21,7 @@ class Tag(models.Model):
         return self.name
 
 class Category(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, null=False, blank=False)
     slug = models.SlugField(unique=True, default=None,
         null=True, blank=True, max_length=255,)
 
@@ -33,12 +34,12 @@ class Category(models.Model):
         return self.name
 
 class Post(models.Model):
-    title = models.CharField(max_length=65)
-    subtitle = models.CharField(max_length=255)
-    status = models.BooleanField(default=False)
-    content = models.TextField()
-    image_in_post_content = models.BooleanField(default=True)
-    image_path = models.ImageField(upload_to=upload_image_to, blank=True, default='')
+    title = models.CharField(max_length=65, null=False, blank=False)
+    subtitle = models.CharField(max_length=255, null=False, blank=False)
+    status = models.BooleanField(default=False, null=False, blank=False)
+    content = models.TextField(null=False, blank=False)
+    image_path = models.CharField(max_length=100)
+    contains_image = models.BooleanField(default=False, null=False, blank=False)
     created_at = models.DateTimeField(default=timezone.now)
     created_by = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='posts_created', null=True)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, default='')
@@ -46,10 +47,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
 
 class PostComments(models.Model):
-    name = models.CharField(max_length=255)
-    content = models.TextField()
+    name = models.CharField(max_length=255, null=False, blank=False)
+    content = models.TextField(null=False, blank=False)
     created_at = models.DateTimeField(default=timezone.now)
     author_id = models.ForeignKey(User, on_delete=models.CASCADE)
     post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
--- blog_mvp/utils/utils_models.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def upload_image_to(instance, filename):
-    post_id = instance.id
-    extension = filename.split('.')[1]
-    new_filename = f'post_{post_id}.{extension}'
-    return os.path.join(str(instance.created_at.year), str(instance.created_at.month), new_filename)
