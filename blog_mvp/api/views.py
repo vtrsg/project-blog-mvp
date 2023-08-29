74f027d15170553e8eb8198f4225c222f8377669
vtrsg <vittorioschmittg@gmail.com>
2023-08-28 21:27:41 -0300

    (feat)[feature/create-view-post]: init view

--- blog_mvp/api/views.py
+++ blog_mvp/api/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from rest_framework.decorators import action
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+# Import models
+from django.contrib.auth.models import User
+from .models import (
+    Tag,
+    Category,
+    Post,
+    PostComments
+)
+# Import serializers
+from .serializers import (
+    UserSerializer,
+    TagSerializer,
+    CategorySerializer,
+    PostSerializer,
+    PostCommentsSerializer
+)
+
+class PostApi(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+    @action(detail=True, methods=['put'])
+    def custom_put(self, request, pk=None):
+        return self.custom_put_method(request, pk=pk)
+
+    @action(detail=True, methods=['delete'])
+    def custom_delete(self, request, pk=None):
+        return self.custom_delete_method(request, pk=pk)
